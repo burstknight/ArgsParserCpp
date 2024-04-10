a578b875468bc0fba4426e14de3e89d3ab9a24f4
burstknight <burstknight@gamil.com>
2024-04-10 22:50:46 +0800

    feat: Update `myArgsParser::addOptionArgument()` to set option arguments
    - Target:
      The method `addOptionArgument()` of the class `myArgsParser` need be
      updated to allow user setting option arguments.
    
    - Changed:
      Update the method `addOptionArgument()` of the class `myArgsParser` to
      set option arguments.

--- src/myArgsParser.cpp
+++ src/myArgsParser.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <memory>
 #include <stdexcept>
+#include <string>
 
 #ifdef _MSC_VER
 	#define __PRETTY_FUNCTION__ 	__FUNCSIG__
@@ -51,21 +52,20 @@ int myArgsParser::addPositionArgument(const std::string& sArgName, const std::st
 } // End of myArgsParser::addPositionArgument
 
 void myArgsParser::addOptionArgument(const std::string& sArgName, const std::string& sDescription, const std::string& sLongArg, char cShortArg, bool isNeedValue, const std::string& sDefaultValue){
+	char acBuffer[BUFFER_SIZE];
+
 	if (true == sArgName.empty()) {
-		char acBuffer[BUFFER_SIZE];
 		snprintf(acBuffer, BUFFER_SIZE, "%s: The parameter `sArgName` cannot be an empty string!", __PRETTY_FUNCTION__);
 		throw invalid_argument(acBuffer);
 	} // End of if-condition
 
 	if (true == sLongArg.empty() && 0 == isalpha(cShortArg)) {
-		char acBuffer[BUFFER_SIZE];
 		snprintf(acBuffer, BUFFER_SIZE, "%s: The parameters `sLongArg` and `cShortArg` are set invalid values meanwhile!", __PRETTY_FUNCTION__);
 		throw invalid_argument(acBuffer);
 	} // End of if-condition
 
 	if (false == sLongArg.empty()) {
 		if (0 != this->m_oLongArguemntSet.count(sLongArg)) {
-			char acBuffer[BUFFER_SIZE];
 			snprintf(acBuffer, BUFFER_SIZE, "%s: The long argument `--%s` for this option argument `%s` has been added!\n", __PRETTY_FUNCTION__, sLongArg.c_str(), sArgName.c_str());
 			throw invalid_argument(acBuffer);
 		} // End of if-condition
@@ -73,10 +73,22 @@ void myArgsParser::addOptionArgument(const std::string& sArgName, const std::str
 
 	if (0 != isalpha(cShortArg)) {
 		if (0 != this->m_oShortArgumentSet.count(cShortArg)) {
-			char acBuffer[BUFFER_SIZE];
 			snprintf(acBuffer, BUFFER_SIZE, "%s: The short argument `-%c` for this option argument `%s` has been added!\n", __PRETTY_FUNCTION__, cShortArg, sArgName.c_str());
 			throw invalid_argument(acBuffer);
 		} // End of if-condition
 	} // End of if-condition
+
+	this->m_oArgNameSet.insert(sArgName);
+	shared_ptr<OptionArgument_s> poOptionArgument = make_shared<OptionArgument_s>();
+	poOptionArgument->sArgName = sArgName;
+	poOptionArgument->isNeedValue = isNeedValue;
+	poOptionArgument->sDefaultValue = sDefaultValue;
+	poOptionArgument->sParsedValue = sDefaultValue;
+	poOptionArgument->sDescription = sDescription;
+	poOptionArgument->sLongArg = string("--") + sLongArg;
+	snprintf(acBuffer, BUFFER_SIZE, "-%c", cShortArg);
+	poOptionArgument->sShortArg = acBuffer;
+
+	this->m_vpoOptionArguments.push_back(poOptionArgument);
 } // End of myArgsParser::addOptionArgument
 
